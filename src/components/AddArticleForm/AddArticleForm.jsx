a0ed29b788bb67508f32be0c556e012a32a577ed
Kyle Wan <kyle94024@gmail.com>
2025-06-20 21:09:19 -0400

    more fix

--- src/components/AddArticleForm/AddArticleForm.jsx
+++ src/components/AddArticleForm/AddArticleForm.jsx
@@ -384,7 +384,7 @@ const AddArticleForm = () => {
                 <Input
                 id="pubDate"
                 type="text"
-                placeholder="e.g. 2025 April 12 (Optional)"
+                placeholder="e.g. 2025 April 12"
                 value={publicationDate}
                 onChange={e => setPublicationDate(e.target.value)}
                 className="add-article-form__input"
